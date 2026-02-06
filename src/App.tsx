ebd68ad7c15871d0292ccbf0c68c90b05fa05183
Ilya Tupitsyn <wwwrt287@gmail.com>
2026-02-07 00:42:57 +0300

    Add static components

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import {AuthProvider} from "./providers/AuthProvider.tsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./components/Home/Home.tsx";
 import Profile from "./components/Profile/Profile.tsx";
+import Contacts from "./components/Contacts/Contacts.tsx";
+import AboutUs from "./components/AboutUs/AboutUs.tsx";
+import Footer from "./components/Footer/Footer.tsx";
 
 const queryClient = new QueryClient();
 
@@ -14,14 +17,16 @@ function App() {
             <BrowserRouter>
                 <AuthProvider>
                     <div className="App">
-                        <Header/>
+                        <Header />
                         <Routes>
                             <Route path="/" element={<Home />} />
                             <Route path="/designs" element={<h1>Designs List (Todo)</h1>}/>
                             <Route path="/profile" element={<Profile />}/>
                             <Route path="/profile/:id" element={<Profile />}/>
+                            <Route path="/contacts" element={<Contacts />}/>
+                            <Route path="/about-us" element={<AboutUs />}/>
                         </Routes>
-
+                        <Footer />
                     </div>
                 </AuthProvider>
             </BrowserRouter>
